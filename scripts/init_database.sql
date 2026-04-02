0fecb5cbdbed4d2fa9f7874239b959f8fb5dd569
Falgunip <63282954+Falgunip@users.noreply.github.com>
2026-04-02 10:12:18 -0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -13,30 +13,48 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
-USE master;
+-- Create Database 'DataWarehouse'
+
+Use Master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+-- Drop and recreate the 'DataWarehouse database
+IF EXISTS (
+          SELECT 1 
+          FROM sys.databases 
+          WHERE name = 'DataWarehouse'
+)
+BEGIN 
+    ALTER DATABASE DataWarehouse 
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
     DROP DATABASE DataWarehouse;
 END;
 GO
 
--- Create the 'DataWarehouse' database
+-- Create the 'DataWarehouse'database again
 CREATE DATABASE DataWarehouse;
 GO
 
-USE DataWarehouse;
+-- Swith to new DB
+Use DataWarehouse;
 GO
 
--- Create Schemas
-CREATE SCHEMA bronze;
+-- Create Schemas safely
+IF NOT EXISTS (SELECT * 
+               FROM sys.schemas
+               WHERE name = 'bronze')
+EXEC('CREATE SCHEMA bronze');
 GO
 
-CREATE SCHEMA silver;
+IF NOT EXISTS (SELECT * 
+               FROM sys.schemas
+               WHERE name = 'silver')
+EXEC('CREATE SCHEMA silver');
 GO
 
-CREATE SCHEMA gold;
+IF NOT EXISTS (SELECT * 
+               FROM sys.schemas
+               WHERE name = 'gold')
+EXEC('CREATE SCHEMA gold');
 GO
